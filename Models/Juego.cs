de5e20f2ae84b998a10e1984eda78413e3812a2d
AUGUSTO PÉREZ RODRIGUEZ <48794823@ort>
2025-09-19 08:19:05 -0300

    AntesDeQueFeliHagaUnMetodo

--- Models/Juego.cs
+++ Models/Juego.cs
@@ -14,7 +14,7 @@ public class Juego
 
     public static List<Respuestas> ListaRespuestas;
 
-     
+
 
     public void IniciarJuego()
     {
@@ -27,15 +27,46 @@ public class Juego
         ListaRespuestas = null;
 
     }
-
-    public  List<Categoria> ObtenerCategoria()
+    public List<Categoria> ObtenerCategorias()
     {
-        List<Categoria>Categorias= BD.ObtenerCategorias();
+        List<Categoria> Categorias = BD.ObtenerCategorias();
         return Categorias;
     }
-    public List<Dificultad> ObtenerDificultad(){
-        List<Dificultad>Dificultades = BD.ObtenerDificultades();
+
+    public List<Dificultad> ObtenerDificultades()
+    {
+        Lista<Dificultad> Dificultades = BD.ObtenerDificultades();
         return Dificultades;
     }
-   
+
+    public void CargarPartida(string username, int dificultad, int categoria)
+    {
+        List<Preguntas> preguntas = BD.ObtenerPreguntas(dificultad, categoria);
+        IniciarJuego();
+        this.username = username;
+        PuntajeActual = 0;
+        CantidadPreguntasCorrectas = 0;
+        ContadorNroPreguntaActual = 0;
+        PreguntaActual = null;
+        ListaPreguntas = preguntas;
+        ListaRespuestas = null;
+    }
+
+    public Preguntas ObtenerProximaPregunta()
+    {
+        if (ContadorNroPreguntaActual <= ListaPreguntas.Count())
+        {
+            return ListaPreguntas[ContadorNroPreguntaActual];
+            ContadorNroPreguntaActual++;
+        }
+        else
+            return null;
+    }
+
+    public List<Preguntas> ObtenerProximasRespuestas(int idPregunta)
+    {
+        List<Respuestas>RespuestasXIdPregunta = BD.ObtenerRespuestas(idPregunta);
+        return RespuestasXIdPregunta;
+    }
 }
+
